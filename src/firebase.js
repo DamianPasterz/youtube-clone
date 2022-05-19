4d4962b4579e0c182b5b6d2447ca0b6649206076
Damian Pasterz <damian.pasterz@gmail.com>
2022-05-19 21:28:28 +0200

    Add css and section in page

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBYH9K9nVMJMioOALjAZECgC_zBnnt_nJw",
+    authDomain: "clone-198ee.firebaseapp.com",
+    projectId: "clone-198ee",
+    storageBucket: "clone-198ee.appspot.com",
+    messagingSenderId: "802014819940",
+    appId: "1:802014819940:web:46897d42cc52407ba29171",
+    measurementId: "G-VG9LVL6TND"
+};
